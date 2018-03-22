107e65690914ff7fdfb59aa476291ac3210d3e6f
CantonTower <1016179795@qq.com>
2018-03-22 14:20:55 +0800

    完成xml基础绘制

---
+++ app/src/main/java/cn/longrise/www/androidprogress/activity/customDrawingActivity/RectangleActivity.java
@@ -0,0 +1,15 @@
+package cn.longrise.www.androidprogress.activity.customDrawingActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.longrise.www.androidprogress.R;
+
+public class RectangleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rectangle);
+    }
+}
--- app/src/main/java/cn/longrise/www/androidprogress/activity/customDrawingActivity/XmlCustomDrawingActivity.java
+++ app/src/main/java/cn/longrise/www/androidprogress/activity/customDrawingActivity/XmlCustomDrawingActivity.java
@@ -10,6 +10,9 @@ import cn.longrise.www.androidprogress.R;
 
 public class XmlCustomDrawingActivity extends AppCompatActivity implements View.OnClickListener{
     private Button mbtnRingDrawing;
+    private Button mbtnRectangleDrawing;
+    private Button mbtnOvalDrawing;
+    private Button mbtnLineDrawing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +24,28 @@ public class XmlCustomDrawingActivity extends AppCompatActivity implements View.
 
     private void initClick() {
         mbtnRingDrawing.setOnClickListener(this);
+        mbtnRectangleDrawing.setOnClickListener(this);
+        mbtnOvalDrawing.setOnClickListener(this);
+        mbtnLineDrawing.setOnClickListener(this);
     }
 
     private void initView() {
         mbtnRingDrawing = (Button) findViewById(R.id.btn_ring_drawing);
+        mbtnRectangleDrawing = (Button) findViewById(R.id.btn_rectangle_drawing);
+        mbtnOvalDrawing = (Button) findViewById(R.id.btn_oval_drawing);
+        mbtnLineDrawing = (Button) findViewById(R.id.btn_line_drawing);
     }
 
     @Override
     public void onClick(View v) {
         if (v == mbtnRingDrawing) {
             startActivity(new Intent(this, RingActivity.class));
+        } else if (v == mbtnRectangleDrawing) {
+            startActivity(new Intent(this, RectangleActivity.class));
+        } else if (v == mbtnOvalDrawing) {
+            startActivity(new Intent(this, OvalActivity.class));
+        } else if (v == mbtnLineDrawing) {
+            startActivity(new Intent(this, LineActivity.class));
         }
     }
 }
